4810f3b4331e343d176d49442a7d97e2fc491ff0
Jitesh Singh <Jitust98@gmail.com>
2021-05-19 23:02:03 +0000

    JPA Configuration

---
+++ src/main/java/com/springboot/basics/UserRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.basics;
+
+import com.springboot.basics.entities.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface  UserRepository extends CrudRepository<User,Integer> {
+}
--- src/main/java/com/springboot/basics/controller/HomeController.java
+++ src/main/java/com/springboot/basics/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.springboot.basics;
+package com.springboot.basics.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
