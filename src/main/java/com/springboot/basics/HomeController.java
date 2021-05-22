4e3922dc2e65224ed90febb138ad7bb896ee97da
Jitesh Singh <Jitust98@gmail.com>
2021-05-22 12:40:59 +0000

    Add DevTools

--- src/main/java/com/springboot/basics/HomeController.java
+++ src/main/java/com/springboot/basics/HomeController.java
@@ -2,6 +2,7 @@ package com.springboot.basics;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,15 @@ public class HomeController {
     @RequestMapping("/")
     public String showHome() {
         return "home";
+    }
+
+    @RequestMapping("/devTools")
+    @ResponseBody
+    public String testDevTool() {
+        int a = 10;
+        int b = 20;
+        int d = 20;
+        int e = 20;
+        return "Hola Bachcho " + (a + b + d + e);
     }
 }
